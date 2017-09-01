2880c335438c428da151f4309644d2ca00a1e82d
TrickyJoker <trickyjoker@TrickyJokers-MacBook-Pro.local>
2017-09-01 17:00:34 +0700

    count post per date

--- avg_tot_like.py
+++ avg_tot_like.py
@@ -12,22 +12,29 @@ def get_year(str_datetime):
 	return str_datetime[0:4]
 
 graph = facebook.GraphAPI(access_token="ACCESS TOKEN", version=2.10)
-post = graph.get_object(id='325477731120719/posts?fields=created_time,likes.limit(0).summary(true),comments.limit(0).summary(true)')
+post = graph.get_object(id='112165272130667/posts?fields=created_time,likes.limit(0).summary(true),comments.limit(0).summary(true)')
 posts_count = len(post['data'])
 likes_count = 0
 comments_count = 0
+num_post_each_date = dict()
 # print(post)
 
 
 for i in  range(len(post['data'])):
 	likes_count += post['data'][i]['likes']['summary']['total_count']
 	comments_count += post['data'][i]['comments']['summary']['total_count']
-	# datetime = post['data'][i]['created_time']
+	datetime = post['data'][i]['created_time']
 	# print(datetime,get_year(datetime),get_month(datetime),get_date(datetime))
+	str_key = str(get_year(datetime)) + '-' + str(get_month(datetime)) + '-' + str(get_date(datetime))
+	if str_key in num_post_each_date:
+		num_post_each_date[str_key] += 1
+	else:
+		num_post_each_date[str_key] = 1
 print('posts_count',posts_count)
 print('likes_count',likes_count)
 print('comments_count',comments_count)
-
+# print('num_post_each_date',num_post_each_date)
+print('date_length',len(num_post_each_date))
 
 
 next_link = post['paging']['next']
@@ -45,6 +52,13 @@ while(len(next_link) != 0):
 	for i in  range(len(r['data'])):
 		likes_count += r['data'][i]['likes']['summary']['total_count']
 		comments_count += r['data'][i]['comments']['summary']['total_count']
+		datetime = r['data'][i]['created_time']
+		
+		str_key = str(get_year(datetime)) + '-' + str(get_month(datetime)) + '-' + str(get_date(datetime))
+		if str_key in num_post_each_date:
+			num_post_each_date[str_key] += 1
+		else:
+			num_post_each_date[str_key] = 1
 	print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<',c,'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 	c += 1
 
@@ -53,6 +67,9 @@ print('likes_count',likes_count)
 print('comments_count',comments_count)
 print('likes_count/posts_count',likes_count/posts_count)
 print('comments_count/posts_count',comments_count/posts_count)
+print('num_post_each_date',num_post_each_date)
+print('date_length',len(num_post_each_date))
+
 
 
 
